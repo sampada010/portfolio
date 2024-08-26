420cf8a3de97f146defe3e8b7018a008b68f2eca
Sampada Tagalpallewar <132118967+sampada010@users.noreply.github.com>
2024-08-26 10:47:11 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+// import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 
 const Contact = () => {
     return (
         <div name="Contact" className="bg-gradient-to-b from-black via-black to-gray-800 w-full min-h-auto text-white gap-4 sm:gap-6 lg:gap-8 px-4 sm:px-6 lg:px-12 py-6 sm:py-8">
-            <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full">
+            <div className="max-w-screen-lg mx-auto p-6 flex flex-col justify-center w-full h-full">
                 <div className='pb-8'>
                     <div className="text-4xl md:text-5xl font-extrabold">
                         <span className="bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">Contact</span>
@@ -21,18 +21,7 @@ const Contact = () => {
                     </form>
                 </div>
 
-                <div className="flex justify-center items-center mt-8">
-                    <a href="https://github.com/sampada010" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-white mx-4 duration-300">
-                        <FaGithub size={30} />
-                    </a>
-                    <a href="https://www.linkedin.com/in/sampada-tagalpallewar-00171625b/" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-white mx-4 duration-300">
-                        <FaLinkedin size={30} />
-                    </a>
-                    <a href="https://mail.google.com/mail/?view=cm&fs=1&to=tagalpallewarsampada10@gmail.com" className="text-gray-500 hover:text-white mx-4 duration-300">
-
-                        <FaEnvelope size={30} />
-                    </a>
-                </div>
+                
             </div>
         </div>
     )
